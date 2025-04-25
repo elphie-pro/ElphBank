151252b4cb917b942b06d291eab96cece777bb54
elphie-pro <onazjr2006@gmail.com>
2025-04-25 15:39:03 +0100

    settings

--- src/components/Account & Settings/head.jsx
+++ src/components/Account & Settings/head.jsx
@@ -11,7 +11,7 @@ export default function Page() {
             <div className="flex justify-between">
                 <div className="flex gap-5">
                     <button  className="cursor-pointer" onClick={() => {route.back()}}><Image src={'/Group 31.svg'} width={45} height={20} alt="s"/></button>
-                    <button className={path === "/account" ? "cursor-pointer" : "hidden"}  onClick={() => {route.push('/')}}><Image src={'/Group 26.svg'} width={50} height={20} alt="s"/></button>
+                    <button className={path === "/account" ? "cursor-pointer" : "hidden"}  onClick={() => {route.push('/settings')}}><Image src={'/Group 26.svg'} width={50} height={20} alt="s"/></button>
                 </div>
                 <div>
                     <h1 className="md:text-[2.3rem] text-[#2ec4b6] font-bold">My Account</h1>
