34464f215751f227245d94a6244bd64b8f16e5e0
elphie-pro <onazjr2006@gmail.com>
2025-04-26 20:19:23 +0100

    balacce

--- src/components/Dashboard/Main.jsx
+++ src/components/Dashboard/Main.jsx
@@ -1,12 +1,38 @@
 "use client"
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { auth, db } from "@/firebase-config";
+import { doc, getDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 
 export default function Page() {
+      const [userData, setUserData] = useState([])
+
+    useEffect(() => {
+        const fetchUserData = () => {
+          onAuthStateChanged(auth, async (user) => {
+            if (user) {
+              const userDoc = doc(db, 'users', user.uid);
+              const docSnap = await getDoc(userDoc);
+      
+              if (docSnap.exists()) {
+                setUserData(docSnap.data());
+              } else {
+                console.log("No such document!");
+              }
+            } else {
+              console.log("No user is logged in");
+            }
+          });
+        };
+      
+        fetchUserData();
+      }, [])
     return (
         <div className="text-black md:ml-[15rem] flex justify-center md:justify-start">
             <div className="flex flex-col gap-14 items-center">
-                <h3 className="text-[1.3rem] font-bold">Your Balance is ₦360,000<span className="text-[#818f96]">.25</span></h3>
+                <h3 className="text-[1.3rem] font-bold">Your Balance is ₦{userData && userData.balance ? userData.balance.toLocaleString() : 'Name'}<span className="text-[#818f96]">.25</span></h3>
                 <div className="pt-8">
                     <Image src='/Group 23.svg' width={300} height={10} alt="pic"/>
                     <div className="absolute mt-[-5.5rem] ml-4 text-white">
