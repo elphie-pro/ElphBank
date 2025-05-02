889cf8d9c9ca30d3272d4edd4b5302ea442075f4
elphie-pro <onazjr2006@gmail.com>
2025-05-02 13:27:26 +0100

    darkmode continue

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -8,8 +8,8 @@ export default function Page() {
     return (
         <div className="absolute mt-[.5rem] ml-[110em] w-[10rem]">
             <button onClick={() => {setTheme(theme === "light" ? "dark" : "light")}} className="cursor-pointer">
-                <Image src='Group 33.svg' width={40} height={20} alt="20" className="absolute rotate-0 scale-100 dark:-rotate-90 dark:scale-0"/>
-                <Image src='Group 34.svg' width={40} height={20} alt="20" className="absolute rotate-90 scale-0 dark:-rotate-0 dark:scale-100"/>
+                <Image src={theme === "light" ? 'Group 33.svg' : 'Group 34.svg'} width={40} height={20} alt="20" className="absolute rotate-90 scale-0 dark:-rotate-0 dark:scale-100"/>
+                {/* <Image src='Group 34.svg' width={40} height={20} alt="20" className="absolute rotate-90 scale-0 dark:-rotate-0 dark:scale-100"/> */}
             </button>
         </div>
     );
