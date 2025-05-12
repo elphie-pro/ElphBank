b0e4f26c436a0e95dceb8575b6fa293e94ef3717
elphie-pro <onazjr2006@gmail.com>
2025-05-12 20:57:48 +0100

    dark tra

--- src/app/help/page.jsx
+++ src/app/help/page.jsx
@@ -11,12 +11,12 @@ export default function Page() {
         show: { opacity: 1, transition: { staggerChildren: 0.25 } },
       };
     return (
-         <motion.div variants={variants} initial="hidden" animate="show" className=' bg-white w-full overflow-hidden min-h-screen'>
+         <motion.div variants={variants} initial="hidden" animate="show" className=' bg-white dark:bg-[#161616] w-full overflow-hidden min-h-screen'>
             <Sidebar />
             <div className="w-full block max-w-[100rem] mx-auto px-4 py-8 pb-1">
                 <h1 className=' md:ml-[20rem] mt-[1rem] absolute text-[2.3rem] text-[#2ec4b6] font-bold'>FAQs</h1>
                 <Buttons />
-                <div className='flex justify-between pt-18 font-black text-black ml-[15rem]'>
+                <div className='flex justify-between pt-18 font-black text-black dark:text-white ml-[15rem]'>
             <div className='flex flex-col gap-4'>
                 <p>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, facere recusandae perferendis ducimus dolorum dolor ipsa repellendus similique eligendi dicta fugit quo ipsum itaque non deleniti voluptatum labore voluptatem praesentium!
