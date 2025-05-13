34cd6fb73b68255aadf6b8a6abf4a27dfeaabd72
elphie-pro <onazjr2006@gmail.com>
2025-05-13 21:47:36 +0100

    darkr

--- src/app/help/page.jsx
+++ src/app/help/page.jsx
@@ -38,9 +38,20 @@ export default function Page() {
 
 
             </div>
+            <div className='flex flex-col gap-4'>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, facere recusandae perferendis ducimus dolorum dolor ipsa repellendus similique eligendi dicta fugit quo ipsum itaque non deleniti voluptatum labore voluptatem praesentium!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora totam laudantium exercitationem dignissimos eos illo quasi libero distinctio natus explicabo. Deserunt veniam hic culpa sit, itaque consectetur, in illum qui, porro sequi quos et nemo quia voluptatum molestias. Tenetur expedita necessitatibus architecto libero totam, quas fugit labore laboriosam iusto illum.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora enim a, sit iure repellendus dolore accusantium culpa qui beatae doloremque mollitia non accusamus, esse aperiam excepturi praesentium explicabo atque similique itaque? Ea nostrum officiis distinctio quae quasi soluta aspernatur tempora, perspiciatis nihil sapiente, dignissimos similique ipsa libero corporis sit doloribus recusandae maiores inventore vel tempore explicabo minima? Nemo veritatis iste iure, sed fugiat non vero soluta et ratione libero magni? Repudiandae sint nesciunt non doloribus eaque hic quos possimus dolorum, excepturi molestiae fugiat rem, obcaecati eum. Cum possimus soluta doloremque?</p>
+
+
+            </div>
             </div>
                 {/* copyright */}
-        <div className="mt-[6rem] ml-[50rem]">
+        <div className="mt-[3rem] ml-[50rem]">
             <p className="md:text-[.9rem] md:ml-[25.3rem]  text-[.7rem] text-[#2EC4BE] font-semibold">© 2025 ElphBank Technologies. All rights reserved</p>
         </div>
             </div>
